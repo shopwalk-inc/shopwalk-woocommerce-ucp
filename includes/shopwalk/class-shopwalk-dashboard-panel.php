shopwalk-inc/woocommerce-ucp

fix(v3.1.1): mask license key + complete uninstall + fix orphan crons
- Mask license key in WP Admin Shopwalk panel (show first 8 + last 4, mid hidden). Bearer credential — must not display in full. Adds Shopwalk_Dashboard_Panel::mask_license_key() helper. Closes F-F-7.
- uninstall.php: add 11 missing option keys (license_needs_activation, plan, plan_label, next_billing_at, license_status, last_heartbeat_at, subscription_status, last_status_poll, discovery_paused, sync_state, sync_history). README FAQ promise of "deletes every shopwalk_* WP option" now actually holds. Closes F-G-1, F-F-12.
- uninstall.php: unschedule shopwalk_status_poll and shopwalk_ucp_direct_checkout_cleanup crons (status_poll missing entirely; direct_checkout cron never registered in either lifecycle hook). Closes F-G-2, F-F-13, half of F-G-3.
- WooCommerce_UCP::deactivate(): add wp_clear_scheduled_hook for shopwalk_ucp_direct_checkout_cleanup. Closes the other half of F-G-3.
- Shopwalk_License::deactivate(): also call Shopwalk_Connect::on_deactivate() so AJAX disconnect doesn't leave the hourly status_poll cron orphaned. Closes F-F-9.

Tests: 47/47 PHPUnit pass.

includes/shopwalk/class-shopwalk-dashboard-panel.php
@@ -41,7 +41,7 @@ final class Shopwalk_Dashboard_Panel {
 			<?php if ( '' !== $license_key ) : ?>
 				<p>
 					<strong><?php esc_html_e( 'License Key:', 'ucp-for-woocommerce' ); ?></strong>
-					<code><?php echo esc_html( $license_key ); ?></code>
+					<code title="<?php esc_attr_e( 'License key is hidden. Find your full key in your Shopwalk partner portal.', 'ucp-for-woocommerce' ); ?>"><?php echo esc_html( self::mask_license_key( $license_key ) ); ?></code>
 				</p>
 			<?php endif; ?>
 			<?php if ( '' !== $pid ) : ?>
@@ -70,5 +70,23 @@ final class Shopwalk_Dashboard_Panel {
 			</p>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Mask a license key for display. Shows first 8 chars + ellipsis + last 4.
+	 *
+	 * The key is a bearer credential — never display it in full. A merchant
+	 * who needs the full key looks it up in the Shopwalk partner portal,
+	 * not here.
+	 *
+	 * @param string $key The license key.
+	 * @return string Masked representation safe to display.
+	 */
+	private static function mask_license_key( string $key ): string {
+		$len = strlen( $key );
+		if ( $len <= 12 ) {
+			return str_repeat( '•', max( 4, $len - 4 ) ) . substr( $key, -4 );
+		}
+		return substr( $key, 0, 8 ) . '…' . substr( $key, -4 );
 	}
 }

uninstall.php
@@ -35,24 +35,50 @@ foreach ( $tables as $name ) {
 }
 
 // ── Delete WP options ───────────────────────────────────────────────────────
+//
+// Keep this list in sync with every option key written by the plugin. The
+// readme FAQ promises "deletes every shopwalk_* WP option"; drift here means
+// orphaned rows in wp_options after uninstall.
 $options = array(
+	// Tier 1 (UCP core).
+	'shopwalk_ucp_gateway_enabled',
+	'shopwalk_ucp_store_signing_secret',
+	// Tier 2 (Shopwalk integration) — license + heartbeat.
 	'shopwalk_license_key',
+	'shopwalk_license_status',
+	'shopwalk_license_needs_activation',
+	'shopwalk_last_heartbeat_at',
 	'shopwalk_partner_id',
+	'shopwalk_plan',
+	'shopwalk_plan_label',
+	'shopwalk_next_billing_at',
+	'shopwalk_subscription_status',
+	'shopwalk_last_status_poll',
+	'shopwalk_discovery_paused',
+	// Tier 2 — sync state.
 	'shopwalk_sync_queue',
+	'shopwalk_sync_state',
+	'shopwalk_sync_history',
+	// Legacy keys from earlier versions — kept for defensive cleanup on
+	// upgrades from <3.0.0; no current writer.
 	'shopwalk_synced_count',
 	'shopwalk_last_sync_at',
 	'shopwalk_notice_dismissed',
-	'shopwalk_ucp_gateway_enabled',
-	'shopwalk_ucp_store_signing_secret',
 );
 foreach ( $options as $option ) {
 	delete_option( $option );
 }
 
 // ── Clear scheduled crons ───────────────────────────────────────────────────
+//
+// Keep in sync with every wp_schedule_(event|single_event) hook the plugin
+// registers. Mirror of WooCommerce_UCP::deactivate() plus any crons whose
+// scheduling lives outside the deactivate path.
 wp_clear_scheduled_hook( 'shopwalk_ucp_session_cleanup' );
 wp_clear_scheduled_hook( 'shopwalk_ucp_webhook_flush' );
+wp_clear_scheduled_hook( 'shopwalk_ucp_direct_checkout_cleanup' );
 wp_clear_scheduled_hook( 'shopwalk_flush_queue' );
+wp_clear_scheduled_hook( 'shopwalk_status_poll' );
 
 // ── Remove /.well-known/ files ─────────────────────────────────────────────
 require_once ABSPATH . 'wp-admin/includes/file.php';

shopwalk-inc/shopwalk-woocommerce-ucp

includes/shopwalk/class-shopwalk-dashboard-panel.php
@@ -41,7 +41,7 @@ final class Shopwalk_Dashboard_Panel {
 			<?php if ( '' !== $license_key ) : ?>
 				<p>
 					<strong><?php esc_html_e( 'License Key:', 'shopwalk-for-woocommerce' ); ?></strong>
-					<code><?php echo esc_html( $license_key ); ?></code>
+					<code title="<?php esc_attr_e( 'License key is hidden. Find your full key in your Shopwalk partner portal.', 'shopwalk-for-woocommerce' ); ?>"><?php echo esc_html( self::mask_license_key( $license_key ) ); ?></code>
 				</p>
 			<?php endif; ?>
 			<?php if ( '' !== $pid ) : ?>
@@ -70,5 +70,23 @@ final class Shopwalk_Dashboard_Panel {
 			</p>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Mask a license key for display. Shows first 8 chars + ellipsis + last 4.
+	 *
+	 * The key is a bearer credential — never display it in full. A merchant
+	 * who needs the full key looks it up in the Shopwalk partner portal,
+	 * not here.
+	 *
+	 * @param string $key The license key.
+	 * @return string Masked representation safe to display.
+	 */
+	private static function mask_license_key( string $key ): string {
+		$len = strlen( $key );
+		if ( $len <= 12 ) {
+			return str_repeat( '•', max( 4, $len - 4 ) ) . substr( $key, -4 );
+		}
+		return substr( $key, 0, 8 ) . '…' . substr( $key, -4 );
 	}
 }

uninstall.php
@@ -35,10 +35,32 @@ foreach ( $tables as $name ) {
 }
 
 // ── Delete WP options ───────────────────────────────────────────────────────
+//
+// Keep this list in sync with every option key written by the plugin. The
+// readme FAQ promises "deletes every shopwalk_* WP option"; drift here means
+// orphaned rows in wp_options after uninstall.
 $options = array(
+	// Tier 1 (UCP core).
+	'shopwalk_ucp_gateway_enabled',
+	'shopwalk_ucp_store_signing_secret',
+	// Tier 2 (Shopwalk integration) — license + heartbeat.
 	'shopwalk_license_key',
+	'shopwalk_license_status',
+	'shopwalk_license_needs_activation',
+	'shopwalk_last_heartbeat_at',
 	'shopwalk_partner_id',
+	'shopwalk_plan',
+	'shopwalk_plan_label',
+	'shopwalk_next_billing_at',
+	'shopwalk_subscription_status',
+	'shopwalk_last_status_poll',
+	'shopwalk_discovery_paused',
+	// Tier 2 — sync state.
 	'shopwalk_sync_queue',
+	'shopwalk_sync_state',
+	'shopwalk_sync_history',
+	// Legacy keys from earlier versions — kept for defensive cleanup on
+	// upgrades from <3.0.0; no current writer.
 	'shopwalk_synced_count',
 	'shopwalk_last_sync_at',
 	'shopwalk_notice_dismissed',
@@ -50,9 +72,15 @@ foreach ( $options as $option ) {
 }
 
 // ── Clear scheduled crons ───────────────────────────────────────────────────
+//
+// Keep in sync with every wp_schedule_(event|single_event) hook the plugin
+// registers. Mirror of WooCommerce_Shopwalk::deactivate() plus any crons whose
+// scheduling lives outside the deactivate path.
 wp_clear_scheduled_hook( 'shopwalk_session_cleanup' );
 wp_clear_scheduled_hook( 'shopwalk_webhook_flush' );
+wp_clear_scheduled_hook( 'shopwalk_direct_checkout_cleanup' );
 wp_clear_scheduled_hook( 'shopwalk_flush_queue' );
+wp_clear_scheduled_hook( 'shopwalk_status_poll' );
 
 // ── Remove /.well-known/ files ─────────────────────────────────────────────
 require_once ABSPATH . 'wp-admin/includes/file.php';